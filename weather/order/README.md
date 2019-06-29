e25110afd6a116e1531a66f7576c4e1acf19affe
Jeff Lyon <jeff@albatrossdigital.com>
2019-06-28 17:59:58 -0700

    Adding weather example

---
+++ weather/order/README.md
@@ -0,0 +1,9 @@
+# Add Weather to Orders
+Use OpenWeatherMap to get the weather forecast at the destination address and add it to the Shopify order's Note Attributes
+
+[![Integrate with Mesa](https://www.getmesa.com/images/integrate.png)<br>Install this library now in Mesa](https://getmesa.com/install/shoppad/mesa-actions/weather/order)
+
+## Setup
+
+1. Create a free OpenWeatherMap and obtain an API Key (`appid`). [Full instructions]()
+2. Paste your key as the `openweathermap-key` [Mesa Secret](https://getmesa.com/go/secrets)
---
+++ weather/order/out-weather-forecast.js
@@ -0,0 +1,49 @@
+const Mesa = require('./vendor/Mesa1.js');
+const OpenWeatherMap = require('./OpenWeatherMap.js');
+
+/**
+ * A Mesa Script exports a class with a script() method.
+ */
+module.exports = new class {
+
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload, context) => {
+    let params = {
+      order_id: payload.order_id
+    };
+
+    // Get weather forecast from OpenWeatherMap
+    const key = Mesa.secret.get('openweathermap-key');
+    const address = payload.shipping_address;
+    const client = new OpenWeatherMap(key);
+    const response = client.getForecast(address.latitude, address.longitude);
+    Mesa.log.info('OpenWeatherMap response', response);
+
+    const weather = response.list.map(item => {
+      const dateString = Mesa.date.format('D, M j Y', item.dt);
+      return {
+        name: `${dateString} Forecast`,
+        value: `${Math.round(item.temp.min)} - ${Math.round(item.temp.max)}°F ${item.weather[0].main}`
+      };
+    });
+
+    let noteAttributes = weather;
+    noteAttributes.concat(payload.note_attributes ? payload.note_attributes : []);
+
+    const data = {
+      "order": {
+        "note_attributes": noteAttributes,
+      }
+    }
+    Mesa.log.info('data', data);
+
+    Mesa.output.done(data, {'order_id': payload.id});
+  }
+}
+
+
