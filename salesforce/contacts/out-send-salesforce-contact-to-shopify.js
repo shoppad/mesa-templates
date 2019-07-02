7b6923dd40d917a0cc3bd2176e88e128e0ea2da0
Noah Wayne <noahwayne84@gmail.com>
2019-07-01 18:15:17 -0700

    initial commit

---
+++ salesforce/contacts/out-send-salesforce-contact-to-shopify.js
@@ -0,0 +1,79 @@
+const Mesa = require('vendor/Mesa1.js');
+const Shopify = require('vendor/Shopify.js');
+const Mapping = require('./Mapping.js');
+const ShopifySalesforceCustomerMap = require('./salesforce/maps/ShopifySalesforceCustomerMap.js');
+
+/**
+ * A Mesa Script exports a class with a script() method.
+ */
+module.exports = new class {
+
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload) => {
+    // Decode payload
+    const decoded = Mesa.xml.decode(payload);
+
+    // Get contact from payload
+    const contact = decoded.soapenv_Body.notifications.Notification.sObject;
+
+    // Get Shopify customer ID
+    const shopifyCustomerId = contact['sf_ShopifyCustomerID__c'];
+
+    // Define processors for convert()
+    const processors = {
+      preProcess: [ this.trimKeys ],
+      process: [ this.updateName ],
+      postProcess: [ this.wrapCustomerOutput ],
+    };
+
+    const shopifyPayload = Mapping.convert(ShopifySalesforceCustomerMap, contact, 'salesforce', 'shopify', processors);
+
+    const response = Shopify.put(`/admin/api/2019-04/customers/${shopifyCustomerId}.json`, shopifyPayload, {
+      debug: true,
+      skipJsonWrap: true,
+    });
+
+    Mesa.log.info('response', response);
+  }
+
+  //
+  // Processor functions, these are called from the Mapping utility's convert() method
+  //
+
+  /**
+   * Remove 'sf_' prefix from keys
+   */
+  trimKeys = (payload) => {
+    let response = {}; 
+
+    Object.keys(payload).forEach(key => {
+      const newKey = key.replace('sf_', '');
+      response[newKey] = payload[key];
+    });
+
+    return response;
+  }
+
+  /**
+   * Wrap customer output for Shopify
+   */
+  wrapCustomerOutput = (payload) => {
+    return { customer: payload };
+  }
+
+  /**
+   * Wrap customer output for Shopify
+   */
+  updateName = (payload, key, input, output, inputValue) => {
+    if (key === 'first_name' && input === 'salesforce' && !inputValue.includes(' H')) {
+      return inputValue + ' H'; 
+    } else {
+      return inputValue;
+    }
+  }
+}
---
+++ salesforce/contacts/out-send-shopify-customer-to-salesforce.js
@@ -0,0 +1,38 @@
+const Mesa = require('./vendor/Mesa1.js');
+const Salesforce = require('./Salesforce.js');
+const Mapping = require('./Mapping.js');
+const ShopifySalesforceCustomerMap = require('./salesforce/maps/ShopifySalesforceCustomerMap.js');
+
+module.exports = new class {
+  script = (payload) => {
+    // Init Salesforce
+    const salesforce = new Salesforce('password', {
+      username: Mesa.secret.get('salesforce_username'),
+      password: Mesa.secret.get('salesforce_password'),
+      client_id: Mesa.secret.get('salesforce_client_id'),
+      client_secret: Mesa.secret.get('salesforce_client_secret')
+    });
+
+    // Convert Shopify payload to SalesForce's contact format
+    const postData = Mapping.convert(ShopifySalesforceCustomerMap, payload, 'shopify', 'salesforce');
+
+    // Base path for Salesforce's contact API
+    const basePath = 'https://na19.salesforce.com/services/data/v20.0/sobjects/Contact';
+
+    // Define an addtional field for Salesforce
+    const externalFieldName = 'ShopifyCustomerID__c';
+    const extenalFieldValue = payload.id;
+
+    // Construct full API path / define options
+    const path = `${basePath}/${externalFieldName}/${extenalFieldValue}`;
+    const options = { include_headers: true };
+
+    // Create or update a contact in Salesforce based on ShopifyCustomerID__c 
+    salesforce.patch(path, postData, options);
+
+    // Get updated contact from Salesforce
+    const salesforceContact = salesforce.get(path, options);
+
+    Mesa.log.info('salesforceContact', salesforceContact);
+  };
+}();
