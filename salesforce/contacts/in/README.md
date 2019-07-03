210d501830d4f237b2b74f00102907c2037f1bbc
Noah Wayne <noahwayne84@gmail.com>
2019-07-03 16:05:30 -0700

    Mesa/salesforce contacts in (#1)
    * initial commit
    
    * remove mapping lib
    
    * rework folder structure
    
    * remove lodash
    
    * remove libraries from local (production now has latest)
    
    * document usage instructions for salesforces contacts in integration
    
    * update syntax for vendor includes
    
    * adjust auth params to match updated auth lib
    
    * add gitignore
    
    * remove unused salesforce files
    
    * remove .DS_store
    
    * update vendor includes
    
    * standardize mesa include / add mesa.json

---
+++ salesforce/contacts/in/README.md
@@ -0,0 +1,40 @@
+# Salesforce - Contacts - In
+Sync Salesforce Contacts to Shopify
+
+[![Integrate with Mesa](https://www.getmesa.com/images/integrate.png)<br>Install this library now in Mesa](https://getmesa.com/install/shoppad/mesa-actions/salesforce/contacts/in)
+
+## Setup
+
+Authorizing with Salesforce (password auth):
+- You will need to create a connected app in Salesforce
+- Click on the Setup buutton (gear icon - top right)
+- Navigate to "Apps", then click "New Connnected App"
+- Enable OAuth Settings, add all scopes
+- Add https://dev-www.theshoppad.com/apps/oauth/ as a callback URL @TODO: what is the production URL?
+- Save the application, then get the client ID and client secret
+- Open up Mesa, then navigate to the Secrets tab
+- Save the client ID, with the key being "salesforce_client_id"
+- Save the client secret, with the key being "salesforce_client_secret"
+- Save your Salesforce username, with the key being "salesforce_username"
+- Save your Salesforce password, with the key being "salesforce_password"
+
+@TODO: document Authorizing with Salesforce (refresh token auth):
+
+Setting up workflows in Salesforce in order to recieve webhooks:
+- Open up your Salesforce instance
+- Click on the Setup buutton (gear icon - top right)
+- Head over to "Process Automation", then to "Workflow Actions"
+- Create an "Outbound Message"
+- Create a "WorkFlow Rule"
+- Tie the "WorkFlow Rule" to the "Outbound Message"
+
+## Developing 
+[Mesa-CLI](https://developers.getmesa.com/cli) command to export code and configuration to your local filesystem:
+
+```
+mesa initialize \
+    --inputs=in-send-salesforce-contact-to-shopify \
+    --outputs=out-send-salesforce-contact-to-shopify \
+    --files=salesforce/in/in-send-salesforce-contact-to-shopify.js,salesforce/in/in-send-salesforce-contact-to-shopify.js \
+    --secrets=salesforce_client_id,salesforce_client_secret,salesforce_username,salesforce_password
+```
---
+++ salesforce/contacts/in/out-send-salesforce-contact-to-shopify.js
@@ -0,0 +1,79 @@
+const Mesa = require('vendor/Mesa.js');
+const Shopify = require('vendor/Shopify.js');
+const Mapping = require('vendor/Mapping.js');
+const ShopifySalesforceCustomerMap = require('./salesforce/maps/shopify-salesforce-customer-map.js');
+
+/**
+ * A Mesa Script exports a class with a script() method.
+ */
+module.exports = new class {
+
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload) => {
+    // Decode payload
+    const decoded = Mesa.xml.decode(payload);
+
+    // Get contact from payload
+    const contact = decoded.soapenv_Body.notifications.Notification.sObject;
+
+    // Get Shopify customer ID
+    const shopifyCustomerId = contact['sf_ShopifyCustomerID__c'];
+
+    // Define processors for convert()
+    const processors = {
+      preProcess: [ this.trimKeys ],
+      process: [ this.updateName ],
+      postProcess: [ this.wrapCustomerOutput ],
+    };
+
+    const shopifyPayload = Mapping.convert(ShopifySalesforceCustomerMap, contact, 'salesforce', 'shopify', processors);
+
+    const response = Shopify.put(`/admin/api/2019-04/customers/${shopifyCustomerId}.json`, shopifyPayload, {
+      debug: true,
+      skipJsonWrap: true,
+    });
+
+    Mesa.log.info('response', response);
+  }
+
+  //
+  // Processor functions, these are called from the Mapping utility's convert() method
+  //
+
+  /**
+   * Remove 'sf_' prefix from keys
+   */
+  trimKeys = (payload) => {
+    let response = {}; 
+
+    Object.keys(payload).forEach(key => {
+      const newKey = key.replace('sf_', '');
+      response[newKey] = payload[key];
+    });
+
+    return response;
+  }
+
+  /**
+   * Wrap customer output for Shopify
+   */
+  wrapCustomerOutput = (payload) => {
+    return { customer: payload };
+  }
+
+  /**
+   * Update user's name
+   */
+  updateName = (fieldKey, inputValue) => {
+    if (fieldKey === 'first_name' && !inputValue.includes('Mr. ')) {
+      return 'Mr. ' + inputValue; 
+    } else {
+      return inputValue;
+    }
+  }
+}
