2e110b6c24a5fb53d8469743971097d267d20377
John Farr <john@theshoppad.com>
2019-07-05 13:55:40 -0700

    Add package for Slack / unfulfilled orders (#5)
    * Slack unfulfilled orders: Add scripts
    
    * Slack unfulfilled orders: Add mesa.json and README.md
    
    * Slack unfulfilled orders: Minor README updates
    
    * Slack unfulfilled orders: Minor comment tweak
    
    * Slack unfulfilled orders: Minor comment tweak
    
    * Slack unfulfilled orders: Remove Salesforce refs, tidy up imports

---
+++ slack/unfulfilled-orders/README.md
@@ -0,0 +1,33 @@
+# Slack - Unfulfilled Orders Alerts
+Send Slack alerts for unfulfilled orders after a configurable period of time
+
+[![Integrate with Mesa](https://www.getmesa.com/images/integrate.png)<br>Install this library now in Mesa](https://getmesa.com/install/shoppad/mesa-actions/slack/unfulfilled-orders)
+
+## Setup
+
+### Slack Setup
+- Install per the link above
+- If you haven't done so previously, you will need to create a connected app in Slack (follow [these steps](https://developers.getmesa.com/libraries/Slack-1.0.0.html))
+- Open up Mesa, then navigate to the Secrets tab
+- Save the incoming Webhook URL as a secret, with the key being "slack-webhook-url"
+- Navigate to the Storage tab
+- Save the slack channel name you wish to use (for example "#unfulfilled-orders"), with the key being "slack-unfulfilled-orders-channel"
+
+### Optional Configuration
+- Navigate to the Outputs tab and expand "Orders Slack Created'
+- Under advanced, change the parameters to filter when orders should be checked for fulfillment
+- For instance, to check for orders at least 1 week old, type "limit=-1&created_at_max={date:7 days ago}". Further details on date syntax can be found under [Virtual Outputs](https://docs.getmesa.com/article/597-outputs#output4)
+- Under Scripts you'll find the file `out-orders-slack-check-fulfilled.js`. By default this script will send a richly formatted Slack notification with buttons and text formatting. 
+- If you prefer a simple Slack message, you can comment out the code in the block "BEGIN Comprehensive Slack request" and uncomment code under "BEGIN Simple slack request (uncomment code below to enable)".
+
+## Developing 
+[Mesa-CLI](https://developers.getmesa.com/cli) command to export code and configuration to your local filesystem:
+
+```
+mesa initialize 
+    --inputs=in-orders-slack-created \
+    --outputs=out-orders-slack-created-vo,out-orders-slack-check-fulfilled \
+    --files=slack/unfulfilled-orders/in-orders-slack-created.js,slack/unfulfilled-orders/out-orders-slack-created.js,slack/unfulfilled-orders/out-orders-slack-check-fulfilled.js \
+    --storage=slack-unfulfilled-orders-channel \
+    --secrets=slack-webhook-url
+```
---
+++ slack/unfulfilled-orders/out-orders-slack-check-fulfilled.js
@@ -0,0 +1,105 @@
+const Mesa = require('vendor/Mesa.js');
+const Slack = require('vendor/Slack.js');
+const Shopify = require('vendor/Shopify.js');
+
+/**
+ * This script processes a custom output containing a created order, doing the following:
+ *
+ *  1. Calls Shopify to check the fulfillment status of the order.
+ *
+ *  2. If order is not fulfilled, sends a Slack notification containing the order details
+ *
+ * Note: There are two Slack snippets here, which can be uncommented / commented out as appropriate:
+ *
+ *  1. The active one uses richly formatted content (https://api.slack.com/docs/message-attachments), with buttons to
+ *     view the order and view all unfulfilled orders
+ *
+ *  2. The second, commented-out version is a simple one-line message.
+ *
+ */
+module.exports = new class {
+
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload, context) => {
+    
+    const url = Mesa.secret.get('slack-webhook-url');
+    const slack = new Slack(url);
+
+    const response = Shopify.get(`admin/orders/${payload.id}.json`);
+
+    const order = response.order;
+
+    // Order is fulfilled, no further action required
+    if (order.fulfillment_status === 'fulfilled') {
+      Mesa.log.info(`Order ${order.name} is fulfilled, no further action necessary`, order.fulfillment_status);
+    }
+
+    // Order is not fulfilled, so send a slack message
+    else if (order.fulfillment_status === null || order.fulfillment_status !== 'fulfilled') {
+      const orderCreatedDate = new Date(order.created_at);
+
+      /** 
+       * BEGIN Comprehensive Slack request, with buttons / text columns
+       *
+       * See below for a simple request version instead
+       */ 
+      slack.send(`Found old unfulfilled order for: https://${context.output.uuid}.myshopify.com`, {
+        channel: Mesa.storage.get('slack-unfulfilled-orders-channel'),
+        attachments: [
+          {
+            'fallback': `Found old unfulfilled order \`${order.name}\`. Order created: \`${Mesa.date.format('D, M j Y', orderCreatedDate.getTime())} \`. https://${context.output.uuid}.myshopify.com/admin/orders/${order.id}`,
+            'title': `Order <https://${context.output.uuid}.myshopify.com/admin/orders/${order.id}|${order.name}>`,
+            'title_link': `https://${context.output.uuid}.myshopify.com/admin/orders/${order.id}`,
+            "fields": [
+              {
+                  'title': 'Date',
+                  'value': Mesa.date.format('D, M j Y', orderCreatedDate.getTime()),
+                  'short': true
+              },
+              {
+                  'title': 'Customer',
+                  'value': order.contact_email,
+                  'short': true
+              }
+            ],
+            'actions': [
+              {
+                'type': 'button',
+                'text': 'Manage order :arrow_right:',
+                'url': `https://${context.output.uuid}.myshopify.com/admin/orders/${order.id}`,
+                'style': 'primary'
+              },
+              {
+                'type': 'button',
+                'text': `View unfulfilled orders`,
+                'url': `https://${context.output.uuid}.myshopify.com/admin/orders?fulfillment_status=unshipped%2Cpartial`,
+              }
+            ],
+          }
+        ],
+      });
+
+      /**
+       * END Comprehensive Slack request
+       */ 
+
+      /**
+        * BEGIN Simple slack request (uncomment code below to enable)
+        */
+
+      //slack.send(`Found old unfulfilled order \`${order.name}\`. Date: \`${Mesa.date.format('D, M j Y', orderCreatedDate.getTime())}\`. <https://${context.output.uuid}.myshopify.com/admin/orders/${order.id}|Manage Order>`, {
+      //   channel: '#jf-slack-test',
+      //   icon_emoji: ':-1:',
+      // });
+
+      /**
+       * END Simple Slack request
+       */ 
+    } 
+  }
+}
