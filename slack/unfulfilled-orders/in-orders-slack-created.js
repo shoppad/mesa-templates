103ad4ae87346c2fe3e0b6482da24a56ae14c4ca
John Farr <john@theshoppad.com>
2019-07-05 11:53:05 -0700

    Slack unfulfilled orders: Add scripts

---
+++ slack/unfulfilled-orders/in-orders-slack-created.js
@@ -0,0 +1,20 @@
+const Mesa = require('./vendor/Mesa1.js');
+
+/**
+ * This script simply places each incoming payload onto a virtual output of the same name
+ *
+ * For example if you have an input with the name "Slack Send", which defaults to the key `in-slack-send`), it will call
+ * the output with the key `out-slack-send`.
+ */
+module.exports = new class {
+
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload, context) => {
+    Mesa.vo.push(`${context.input.key.replace('in-', 'out-')}-vo`, payload);
+  }
+}
