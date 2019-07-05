71dfecc8a4b16cdfbac652bb44876475bec1ef25
Darryl Norris <admin@darrylnorris.com>
2019-07-05 16:41:05 -0700

    Google Sheets Customer Update

---
+++ google/customer-update/customer/out-google-sheets-update.js
@@ -0,0 +1,38 @@
+const Mesa = require("vendor/Mesa.js");
+const Google = require("vendor/Google.js");
+
+/**
+ * A Mesa Script exports a class with a script() method.
+ */
+module.exports = new class {
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload, context) => {
+    // Initliazing Google Class.
+    let google = new Google(
+      payload.google.init.grandType,
+      payload.google.init.secretKeys
+    );
+
+    // Updating the google sheets payload.
+    payload.google.sheets.payload = google.sheets.addOrUpdate(
+      [
+        payload.shopify.id,
+        `${payload.shopify.first_name} ${payload.shopify.last_name}`,
+        payload.shopify.email,
+        payload.shopify.phone
+      ],
+      payload.google.sheets.payload,
+      0,
+      "customer_id",
+      payload.shopify.id
+    );
+
+    Mesa.log.info("Google Sheets Update", payload);
+    Mesa.output.send("out-customer-update", payload);
+  };
+}();
