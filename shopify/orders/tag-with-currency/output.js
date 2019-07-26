920c23b945e22771e7c99a042d794711cf8e0fa0
Aaron Wadler <aaron@theshoppad.com>
2019-07-26 16:18:50 -0700

    New template

---
+++ shopify/orders/tag-with-currency/output.js
@@ -0,0 +1,26 @@
+const Mesa = require('vendor/Mesa.js');
+
+/**
+ * A Mesa Script exports a class with a script() method.
+ */
+module.exports = new class {
+
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload, context) => {
+    
+    const tags = payload.tags + ',' + payload.currency;
+    
+    const data = {
+      "order": {
+        "tags": tags
+      }
+    };
+
+    Mesa.output.done(data, {'order_id': payload.id});
+  }
+};
