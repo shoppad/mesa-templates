e346666d5807292f93e89a9dd77e47cc440f54f1
Jeff Lyon <jeff@albatrossdigital.com>
2019-08-15 08:51:57 -0700

    rm copyright

--- shopify/order/add-shipping-province-tag/in-shopify-order-webhook.js
+++ shopify/order/add-shipping-province-tag/in-shopify-order-webhook.js
@@ -1,22 +1,3 @@
-/*******************************************************************************
- *
- * SHOPPAD CONFIDENTIAL
- * __________________
- *
- * Copyright (c) 2012 - 2019 ShopPad Inc.
- * All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of ShopPad Inc. and its suppliers, if any.
- * The intellectual and technical concepts contained herein are
- * proprietary to ShopPad Inc. and its suppliers and may be covered
- * by U.S. and Foreign Patents, patents in process, and are protected
- * by trade secret or copyright law. Dissemination of this information
- * or reproduction of this material is strictly forbidden unless prior
- * written permission is obtained from ShopPad Inc.
- *
- ******************************************************************************/
-
 const Mesa = require('vendor/Mesa.js');
 
 /**
