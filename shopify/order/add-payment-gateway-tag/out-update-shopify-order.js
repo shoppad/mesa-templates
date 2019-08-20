b9c7c0a1ce3069555553b65c024fb03a77ca0a51
Aaron Wadler <aaronwadler@aarons-mbp-2.lan>
2019-08-19 19:52:38 -0700

    Remove payload state dependency and add debug flag

--- shopify/order/add-payment-gateway-tag/out-update-shopify-order.js
+++ shopify/order/add-payment-gateway-tag/out-update-shopify-order.js
@@ -1,4 +1,5 @@
 const Mesa = require('vendor/Mesa.js');
+const Shopify = require('vendor/Shopify.js');
 
 /**
  * A Mesa Script exports a class with a script() method.
@@ -12,7 +13,17 @@ module.exports = new class {
    * @param {object} context Additional context about this task
    */
   script = (payload, context) => {
-    const tags = payload.tags + "," + payload.gateway;
+
+    let debug = Mesa.storage.get('debug', false);
+    debug = !!debug;
+
+    const order = Shopify.get('/admin/orders/' + payload.id + '.json', {debug: debug});
+
+    if (debug) {
+      Mesa.log.info('Order:', order);
+    }
+
+    const tags = order.tags ? order.tags + "," + payload.gateway : payload.gateway;
 
     const data = {
       order: {
