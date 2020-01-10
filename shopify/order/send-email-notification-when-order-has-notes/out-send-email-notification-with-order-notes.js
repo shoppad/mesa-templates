243cb50fe028b4e14659d8d2a536631bb6d6875f
Anh Pham <anhpham@Anhs-MacBook-Pro.local>
2020-01-10 14:12:59 -0800

    Update email formatting and adjusted logic of template

--- shopify/order/send-email-notification-when-order-has-notes/out-send-email-notification-with-order-notes.js
+++ shopify/order/send-email-notification-when-order-has-notes/out-send-email-notification-with-order-notes.js
@@ -15,53 +15,44 @@ module.exports = new (class {
     Mesa.log.debug('Payload', payload);
     Mesa.log.debug('Context', context);
 
-    const customEmailAddress = Mesa.storage.get('email-address');
-    const storeEmailAddress = context.shop.email;
-
-    const orderNumber = payload.name;
     const orderNotes = payload.note;
 
-    Mesa.log.debug(
-      'Email address in storage',
-      customEmailAddress
-        ? customEmailAddress
-        : 'No email address specified in storage'
-    );
-
-    // if order has notes in payload
+    // if order notes is not empty
     if (orderNotes != '') {
+      let emailAddress = context.shop.email;
+      const orderNumber = payload.name;
       // if email not specified in storage, send to store owner
       // else send to email address specified in storage
-      if (!customEmailAddress) {
-        Mesa.log.debug('Using store email address');
-        Mesa.log.debug('Store email address', storeEmailAddress);
 
-        // Send email
-        Mesa.email.send(
-          storeEmailAddress,
-          'Order ' + orderNumber + ' created with notes',
-          'Hello ' +
-            context.shop.name +
-            ', \n' +
-            payload.customer.first_name +
-            ' ' +
-            payload.customer.last_name +
-            ' placed a new order with your store. \n' +
-            // List products in order
-            'There are notes included. ' +
-            orderNotes
-        );
+      if (Mesa.storage.get('email-address')) {
+        emailAddress = Mesa.storage.get('email-address');
+        Mesa.log.debug('Using custom email address', emailAddress);
       } else {
-        Mesa.log.debug('Using email address specified in storage');
-        Mesa.log.debug('Custom email address', customEmailAddress);
-
-        // Send email
-        Mesa.email.send(
-          customEmailAddress,
-          'Order ' + orderNumber + ' created with notes',
-          'Notes included: ' + orderNotes
-        );
+        Mesa.log.debug('Using store email address', emailAddress);
       }
+
+      // let emailBody = `Hello! ${emailAddress}`;
+
+      // Send email
+      Mesa.email.send(
+        emailAddress,
+        'Order ' + orderNumber + ' created with notes',
+        'Hello ' +
+          context.shop.name +
+          ', \n \n' +
+          payload.customer.first_name +
+          ' ' +
+          payload.customer.last_name +
+          ' placed a new order with your store. \n Notes have been included in the order. They are the following: \n \n' +
+          orderNotes +
+          '\n \n View Order ' +
+          orderNumber +
+          ': ' +
+          'https://' +
+          context.shop.domain +
+          '/admin/orders/' +
+          payload.id
+      );
     }
   };
 })();
