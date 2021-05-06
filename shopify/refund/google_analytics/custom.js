3beb38e0d02c94641a5aa1a1805a3d9ee45bc6e1
Anh Pham <anh@theshoppad.com>
2021-05-06 14:29:55 -0700

    Add google_analytics template (#207)
    * Add shopify/refund/google_analytics template
    
    * Add shopify/refund/google_analytics template
    
    * Remove Google Analytics ID
    
    * Remove Mapping step

---
+++ shopify/refund/google_analytics/custom.js
@@ -0,0 +1,28 @@
+const Mesa = require('vendor/Mesa.js');
+
+/**
+ * A Mesa Script exports a class with a script() method.
+ */
+module.exports = new class {
+
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload, context) => {
+
+    // Loop through transactions on the refund, and total up the refund amount
+     let refundAmount = 0;
+     payload.transactions.forEach(transaction => {
+       refundAmount -= transaction.amount;
+     });
+     
+     // Add to payload
+     payload.total_refund_amount = refundAmount;
+    
+    // We're done, call the next step!
+    Mesa.output.next(payload);
+  }
+}
