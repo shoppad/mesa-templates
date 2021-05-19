af83ff1f481aaa78729052739834d3a9c7138f39
Mandy Hom <amandakhom@gmail.com>
2021-05-19 13:27:05 -0700

    Adding Google Sheets template

---
+++ tracktor/order/send_to_google_sheets/custom.js
@@ -0,0 +1,40 @@
+const Mesa = require('vendor/Mesa.js');
+
+/**
+ * A Mesa Script exports a class with a script() method.
+ */
+module.exports = new class {
+
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload, context) => {
+
+    // Add your custom code here
+
+    // Setting variable
+    let isReadyToSendOrder = true;
+
+    // Looping through each fulfillment item
+    payload.line_items.forEach((item)=> {
+        // Checking if fulfillment is fulfilled and requires shipping
+        Mesa.log.info('item', item)
+        if (item.fulfillment_status && item.fulfilllment_status != 'fullfiled' && item.requires_shipping == false) {
+            // If so, set variable to false
+               Mesa.log.info('false')
+            isReadyToSendOrder = false;
+        }
+    })
+
+    // If variable is true, proceed to next Step!
+    if (isReadyToSendOrder) {
+        Mesa.output.next(payload);
+    // If false, log this statement.
+    } else {
+        Mesa.log.info('One of the packages in the order is not fulfilled or requires shipping');
+    }
+}
+}
