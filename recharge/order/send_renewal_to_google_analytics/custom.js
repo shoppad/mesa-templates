fa3d9648297ba7ab8a9aa595fe19690557f0a661
Anh Pham <anh@theshoppad.com>
2022-06-21 09:05:49 -0700

    Remove console.log statements in custom.js

--- recharge/order/send_renewal_to_google_analytics/custom.js
+++ recharge/order/send_renewal_to_google_analytics/custom.js
@@ -14,12 +14,10 @@ module.exports = new (class {
     // Add your custom code here
     // Add client ID from note attributes
     if (payload.note_attributes) {
-      Mesa.log.info('Has payload.note_attributes');
       const result = payload.note_attributes.filter(
         (attribute) => attribute.name == "cid"
       );
       if (result && result.length > 0) {
-         Mesa.log.info('result[0].value', result[0].value);
         payload.cid = result[0].value;
       }
     }
