bbf65ac4a3bd376407f71b37d0a96c1a72990fa8
Anh Pham <anh@theshoppad.com>
2022-11-30 14:18:10 -0800

    Update Filter step in Tag an order when expedited shipping is selected template

--- shopify/order/send_expedited_shipping_alert_to_logistics/filter.js
+++ shopify/order/send_expedited_shipping_alert_to_logistics/filter.js
@@ -1,4 +1,5 @@
 const Mesa = require('vendor/Mesa.js');
+const Filter = require('vendor/Filter.js');
 
 /**
  * A Mesa Script exports a class with a script() method.
@@ -13,25 +14,18 @@ module.exports = new class {
    */
   script = (payload, context) => {
 
-    Mesa.log.debug("metadata", context.trigger.metadata);
+    let {a, b, comparison, additional} = context.trigger.metadata;
 
-    let needle = context.trigger.metadata.a;
-    let haystack = context.trigger.metadata.b;
-
-    haystack = haystack.split(",").map(function(e){return e.toLowerCase().trim()});
-    haystack = haystack.filter(function(elem) {
-      return needle.toLowerCase().indexOf(elem) >= 0;
-    });
-
-    if (haystack.length) {
+    if (Filter.process(a, b, comparison, additional)) {
       // Passed the filter, call the next step and pass the original payload
-      Mesa.log.debug(`Conditions passed: ${needle} is in ${haystack}`);
+      Mesa.log.debug(`Conditions passed: ${Filter.stringify(a, b, comparison, additional)}`);
       Mesa.output.next(payload);
     }
     else {
       // Did not pass the filter, stop execution by doing nothing
       // Alternatively add code here if you would like something to happen when the conditions do not pass
-      Mesa.log.warn(`Conditions (${needle} in ${haystack}) did not pass, stopping execution`);
+      Mesa.log.warn(`Conditions (${Filter.stringify(a, b, comparison, additional)}) did not pass, stopping execution`);
+      Mesa.log.debug('Full configuration', context.trigger.metadata);
     }
   }
 
