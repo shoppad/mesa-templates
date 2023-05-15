13113fdedbba302a4e75ba135efc568a5bd5b889
Kalen Jordan <kalen.jordan@kalenjo6mk6mx9j.lan>
2023-05-15 08:57:04 -0500

    Sync images to airtable

---
+++ unsearchable/shopify/product/sync_to_airtable/custom_1.js
@@ -0,0 +1,49 @@
+const Mesa = require('vendor/Mesa.js');
+
+/**
+ * A Mesa Script exports a class with a script() method.
+ */
+module.exports = new class {
+
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload, context) => {
+
+    // Add your custom code here
+    let options = {
+      "headers": {
+        "Content-Type": "application\/json",
+        "Authorization": "Bearer " + Mesa.credential.get('airtable'),
+      }
+    }
+
+    let productId = context.steps.loop.current_item.id;
+    Mesa.log.info("context steps custom", context.steps.custom);
+
+    let recordId = context.steps.custom.record.id;
+    let filterByFormula = `{Product ID} = '${productId}'`;
+    let base = context.steps.transform.Base;
+    let table = context.steps.transform.Table;
+    let url = 'https://api.airtable.com/v0/' + base + '/' + table + '/' + recordId;
+
+    let data = {
+        "fields": {
+            "Image": [
+              {
+                "url": context.steps.loop.current_item.src,
+              }
+            ],
+        }
+      }
+
+    let results = Mesa.request.patch(url, data, options);    
+    Mesa.log.info("results", results);
+
+    // We're done, call the next step!
+    Mesa.output.next(payload);
+  }
+}
