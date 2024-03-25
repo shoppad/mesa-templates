045377b5030b6c14c6ae0ecad2f2309bb6fcefe4
Kalen Jordan <kalen.jordan@kalenjo6mk6mx9j.lan>
2024-03-25 08:34:14 -0500

    Search & Discovery Boosts Metafield template

---
+++ shopify/product/search_and_discovery_metafields/custom.js
@@ -0,0 +1,30 @@
+const Mesa = require('vendor/Mesa.js');
+
+/**
+ * A Mesa Script exports a class with a script() method.
+ */
+module.exports = new class {
+
+  /**
+   * Mesa Script
+   *
+   * @param {object} payload The payload data
+   * @param {object} context Additional context about this task
+   */
+  script = (payload, context) => {
+    const vars = context.steps;
+
+    let boostValues = JSON.parse(vars.shopify_1.value);
+    let newValue = vars.shopify_3.value;
+    boostValues.push(newValue);
+
+    // Get unique values so there aren't duplicates
+    boostValues = [...new Set(boostValues)];
+
+    Mesa.trigger.setTaskExternalData({
+      "label": boostValues.join(', ')
+    })
+
+    Mesa.output.next(boostValues);
+  }
+}
