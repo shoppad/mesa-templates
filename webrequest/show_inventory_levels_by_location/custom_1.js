8f8d261b9e85cfa5386efbd5c11ed93b2d221a90
Daniel White <danielfwhite1@gmail.com>
2025-01-29 17:55:12 -0800

    Removed line 3 and 33 in custom_1.js and removed description and seconds in mesa.json, also removed - in web-request directory

--- webrequest/show_inventory_levels_by_location/custom_1.js
+++ webrequest/show_inventory_levels_by_location/custom_1.js
@@ -1,6 +1,5 @@
 const Mesa = require('vendor/Mesa.js');
 const ShopifyGraphql = require('vendor/ShopifyGraphql.js');
-const Shopify= require('vendor/Shopify.js');
 
 /**
  * A Mesa Script exports a class with a script() method.
@@ -30,7 +29,6 @@ module.exports = new class {
       }
     `;
 
-    // const r = ShopifyGraphql.send(query, null, {}, 'admin/api/2023-10/graphql.json');
     const r = ShopifyGraphql.send(query, null);
 
     Mesa.output.next({"response": r.data.productVariant.inventoryItem.inventoryLevels.nodes});
