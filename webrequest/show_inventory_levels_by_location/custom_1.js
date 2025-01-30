0f45818c9a9545b2fd2785f96222afeb6018266a
Daniel White <danielfwhite1@gmail.com>
2025-01-30 09:09:31 -0800

    Add Template: Show Inventory Levels By Location On Your Product Page (#639)
    * Add Template: Show Inventory Levels By Location On Your Product Page
    
    * Removed zip file and replaced with folder containing template files
    
    * Removed line 3 and 33 in custom_1.js and removed description and seconds in mesa.json, also removed - in web-request directory
    
    * Changed setup in mesa.json to false

---
+++ webrequest/show_inventory_levels_by_location/custom_1.js
@@ -0,0 +1,36 @@
+const Mesa = require('vendor/Mesa.js');
+const ShopifyGraphql = require('vendor/ShopifyGraphql.js');
+
+/**
+ * A Mesa Script exports a class with a script() method.
+ */
+module.exports = new class {
+  script = (payload, context) => {
+    let vars = context.steps;
+    let variantId = vars.webrequest.querystring.variant_id;
+    
+    let query = `
+      {
+        productVariant(id:"gid://shopify/ProductVariant/${variantId}") {
+          sku
+          inventoryItem {
+            inventoryLevels(first:10) {
+              nodes {
+                location {
+                  name
+                }
+                quantities(names:"available") {
+                  available: quantity
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    const r = ShopifyGraphql.send(query, null);
+
+    Mesa.output.next({"response": r.data.productVariant.inventoryItem.inventoryLevels.nodes});
+  }
+}
