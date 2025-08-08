3caa2e6b906cd3d33cb0d0fa78ba0a232a27c163
Anh Pham <anh@theshoppad.com>
2025-08-08 14:56:59 -0700

    Update template collection: Sync inventory between Shopify stores (#807)
    * Update template collection: Sync inventory between Shopify stores
    
    * Update custom code

--- shopify/inventory/sync_inventory_between_stores_kj/send/send_webhook.js
+++ shopify/inventory/sync_inventory_between_stores_kj/send/send_webhook.js
@@ -14,6 +14,9 @@ module.exports = new class {
   script = (payload, context) => {
     const vars = context.steps;
 
+    // Get Shopify shop details
+    const shopifyShopDetails = vars.shopify;
+
     let output = context.automation.outputs.find(object => object.key == 'send_webhook');
     let webhookUrl = output.metadata.webhook_url;
 
@@ -23,7 +26,7 @@ module.exports = new class {
 
     Mesa.request.post(webhookUrl, {
       "sku": vars.loop.sku,
-      "shop_name": context.shop.name,
+      "shop_name": shopifyShopDetails.name,
       "location": vars.loop_1.location.name,
       "delta": vars.custom_1.delta,
       "run_time": vars.custom.run_time,
