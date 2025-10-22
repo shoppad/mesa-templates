96d501d80fb8dfe141b0d683214f744773c629c3
Anh Pham <anh@theshoppad.com>
2025-10-22 14:55:28 -0700

    Update template titled Export Delivered Order and Fulfillment Details to Google Sheets (#867)

--- tracktor/order/send_to_google_sheets/custom.js
+++ tracktor/order/send_to_google_sheets/custom.js
@@ -1,40 +1,43 @@
-const Mesa = require('vendor/Mesa.js');
+const Mesa = require("vendor/Mesa.js");
 
 /**
- * A Mesa Script exports a class with a script() method.
+ * A MESA Script exports a class with a script() method.
  */
-module.exports = new class {
-
+module.exports = new (class {
   /**
-   * Mesa Script
+   * MESA Script
    *
-   * @param {object} payload The payload data
+   * @param {object} prevResponse The response from the previous step
    * @param {object} context Additional context about this task
    */
-  script = (payload, context) => {
-
-    // Add your custom code here
+  script = (prevResponse, context) => {
+    // Retrieve the Variables Available to this step
+    const vars = context.steps;
 
     // Setting variable
     let isReadyToSendOrder = true;
+    // Get Shopify order
+    let shopifyOrder = vars.shopify;
 
     // Looping through each fulfillment item
-    payload.line_items.forEach((item)=> {
-        // Checking if fulfillment is fulfilled and requires shipping
-        Mesa.log.info('item', item)
-        if (item.fulfillment_status == null && item.requires_shipping == true) {
-            // If so, set variable to false
-            Mesa.log.info('false')
-            isReadyToSendOrder = false;
-        }
-    })
+    shopifyOrder.line_items.forEach(item => {
+      // Checking if fulfillment is fulfilled and requires shipping
+      Mesa.log.info("item", item);
+      if (item.fulfillment_status == null && item.requires_shipping == true) {
+        // If so, set variable to false
+        Mesa.log.info("false");
+        isReadyToSendOrder = false;
+      }
+    });
 
-    // If variable is true, proceed to next Step!
+    // If variable is true, proceed to next step
     if (isReadyToSendOrder) {
-        Mesa.output.next(payload);
-    // If false, log this statement.
+      Mesa.output.next(shopifyOrder);
+      // If false, log this statement.
     } else {
-        Mesa.log.info('One of the packages in the order is not fulfilled or requires shipping');
+      Mesa.log.info(
+        "One of the packages in the order is not fulfilled or requires shipping"
+      );
     }
-}
-}
+  };
+})();
